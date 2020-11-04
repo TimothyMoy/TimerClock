3e9803ba19923ed9305195f935a3d55f5e681b07
TimothyMoy <Timothy.z.moy@gmail.com>
2020-11-04 11:09:35 -0800

    created conditional and set intervals to update the time

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
 function showTime(){
+  //Create a variable to hold Date object
   const date = new Date();
-  const hour = date.getHours();
-  const min = date.getMinutes();
-  let sec = date.getSeconds();
 
-
-  document.getElementById("hour").innerHTML = hour;
-  document.getElementById("minute").innerHTML = min;
-  document.getElementById("second").innerHTML = sec;
-
+  //Create variable to get hours, minutes, and seconds
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  
+  //Conditionals have to be set before manipulating the dom, if time is below 10, then we need to add a 0
   if (hours < 10) {
-    hour = "0" + hour;
+    hours = "0" + hours;
   }
 
+  if (mins < 10) {
+    mins = "0" + mins;
+  }
+
+  if (secs < 10) {
+    secs = "0" + secs;
+  }
+
+  //Get ID and manipulate the DOM with the variables set earlier.
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = ": " + mins;
+  document.getElementById("second").innerHTML = ": " + secs;
 }
-showTime();
 
-setInterval(showTime, 100)
+//Setting a time interval so that the time would update by itself.
+setInterval(showTime, 1000);
 
-console.log('hello world')
